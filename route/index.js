48cafbac28b806ad07e7ef718458814adef8116c
Rohit Pednekar <rohitpednekar95@gmail.com>
2019-08-09 01:38:20 +0530

    Debugging Succeded

--- route/index.js
+++ route/index.js
@@ -48,7 +48,7 @@ router.get("/",async (req,res)=>{
           });
       }else {
         if(req.file) {
-          console.log("----------------1-----------------")
+          
           var imgArr = req.file.originalname.split('.');
                   var format = imgArr[imgArr.length - 1].trim();
                   if(format == 'pdf' || format == 'docs' || format == 'doc' || format == 'docx') {					
@@ -56,39 +56,27 @@ router.get("/",async (req,res)=>{
                       var fs = require('fs');
             var mkdirp = require('mkdirp');
             var shortidFolder = require('shortid').generate();
-            console.log("----------------2-----------------")
+  
                   var uploadResume = new Promise((resolve,reject)=>{
-                    console.log("----------------3-----------------")
                       //Make id folder in the upload directory
                       mkdirp(root_path+constant.UploadPath+shortidFolder, function (err) {
-                        console.log("----------------4-----------------")
                         if (err){
-                          console.log("----------------5-----------------")
                           fs.unlink(file.path,(error)=>{
                             reject(err);
                           });
-                          console.log("----------------6-----------------")
                           reject(err);
-                          console.log("----------------7-----------------")
                         }else {
-                          console.log("----------------8-----------------")
-                          fs.readFile(req.file.path,'utf8', function (err, data) {
-                            console.log("----------------9-----------------")
+                          fs.readFile(req.file.path, function (err, data) {
                             var newPath = root_path+constant.UploadPath+shortidFolder+'/'+req.file.originalname;
                             fs.writeFile(newPath, data, function (err) {
-                              console.log("----------------10-----------------")
                               //delete the temporary file
                               fs.unlink(req.file.path,(error)=>{
                                 reject(err);
                               });
-                              console.log("----------------11-----------------")
                                 if(err) {
-                                  console.log("----------------12-----------------")
                                   reject(err);
-                                  console.log("----------------13-----------------")
                                   
                                 }else {
-                                  console.log("----------------14-----------------")
                                   resolve();
                                 }
                               });
@@ -98,7 +86,6 @@ router.get("/",async (req,res)=>{
                       });
   
                   uploadResume.then((data)=>{
-                    console.log("----------------15-----------------")
                     var shortid =  shortidFolder+ "/"+req.file.originalname;
                       var user = {
                             Name: req.body.name,
@@ -107,13 +94,10 @@ router.get("/",async (req,res)=>{
                             PhoneNumber: req.body.phoneNumber,
                             ResumeLocation : shortid
                         };
-                        console.log("----------------16-----------------")
+                      
                         var connectionEst = MongoDatabase.connection().then((mongoConnectionResult)=>{
-                          console.log("----------------17-----------------")
                           var inserted = mongoConnectionResult.mongoConnectionObj.collection('users').insertOne(user).then((insertionSucced)=>{
-                            console.log("----------------18-----------------")
                             mongoConnectionResult.mongoConnection.close();
-                            console.log("----------------19-----------------")
                                 res.json({
                                   status: 200,
                                   message: 'Data successfully inserted !',
